87c16a073aaf7b23d8af96b91a7e3ab5dbfc9697
Craig Fletcher <craig.fletcher@onthebeach.co.uk>
2013-12-03 19:20:33 +0000

    Holy shit, I actually got parallax working.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>leakypixel</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+  </head>
+  <body>
+    <header data-speed="10" data-type="background">
+      <h1>leakypixel</h1>
+    </header>
+    <header data-speed="100" data-type="background">
+      <h1>leakypixel</h1>
+    </header>
+    <header data-speed="1000" data-type="background">
+      <h1>leakypixel</h1>
+    </header>
+    <section></section>
+    <footer></footer>
+    <?php include "assets/js/vendor.php"; ?>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/parallax.js"></script>
+  </body>
+</html>
+

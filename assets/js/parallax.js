0e3558f3375d89a2ccabdeb74de5f4c744e2c4c3
Craig Fletcher <craig.fletcher@onthebeach.co.uk>
2014-02-18 17:09:33 +0000

    Added some parallax things.

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     // Cache the Window object for performance reasons.
     $window = $(window);
-    $('[data-scrollspeed]').each(function(){
-      var $bgobj = $(this); // assigning the object
+    var scrollElms = document.querySelectorAll("[data-scrollspeed]");
+    console.log(scrollElms.length);
+      for (var x = 0; x < scrollElms.length; x++) {
+      var currentElm = this; // assigning the object
       $(window).scroll(function() {
         // Scroll the background at var speed
         // the yPos is a negative value because we're scrolling it UP! 
-        var yPos = -($window.scrollTop() * $bgobj.data('scrollspeed')); 
+        var yPos = -($window.scrollTop() * currentElm.data('scrollspeed')); 
         // Put together our final background position
         var coords = '50% '+ yPos + 'px';
         // Move the background
-        $bgobj.css({ backgroundPosition: coords });
+        $bgobj.style.backgroundPosition = coords;
       }); // window scroll Ends
     });
 });
--- index.php
+++ index.php
@@ -7,12 +7,13 @@
     <link rel="stylesheet" href="assets/css/home.css">
   </head>
   <body>
-    <header data-scrollspeed="0.1">
+    <header data-scrollspeed="2">
       <h1>leakypixel</h1>
     </header>
     <section></section>
     <footer></footer>
     <?php include "assets/js/vendor.php"; ?>
+    <?php include "/assets/js/vendor.php"; ?>
     <script src="assets/js/main.js"></script>
     <script src="assets/js/parallax.js"></script>
   </body>

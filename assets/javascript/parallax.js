34f98b514e5b84e38129bd01c29a6a35daceb41f
Craig Fletcher <craig.fletcher@onthebeach.co.uk>
2014-06-06 11:07:06 +0100

    jshint working and parallax.js linted.

--- assets/javascript/parallax.js
+++ assets/javascript/parallax.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
+$(document).ready(
+  function(){
     $window = $(window);
     var scrollElms = document.querySelectorAll("[data-scrollspeed]");
-    console.log(scrollElms.length);
-      for (var x = 0; x < scrollElms.length; x++) {
-      var currentElm = this; 
-      $(window).scroll(function() {
-        var yPos = -($window.scrollTop() * currentElm.data('scrollspeed'));
-        var coords = '50% '+ yPos + 'px';
-        $bgobj.style.backgroundPosition = coords;
-      });
-    });
-});
+    function scrollTo(currentElm) {
+      var yPos = -($window.scrollTop() * currentElm.data('scrollspeed'));
+      var coords = '50% '+ yPos + 'px';
+      $bgobj.style.backgroundPosition = coords;
+    }
+    for (var x = 0; x < scrollElms.length; x++) {
+      var currentElm = this;
+      $(window).scroll(scrollTo(currentElm));
+    }
+  }
+);
--- gruntfile.js
+++ gruntfile.js
@@ -13,15 +13,30 @@ module.exports = function(grunt) {
       }
     },
     jshint: {
-      all: ['Gruntfile.js', 'assets/javascript/*.js']
+      all: {
+        src: ['assets/javascript/*.js']
+      },
     },
     watch: {
       css: {
         files: 'assets/sass/*.scss',
         tasks: ['sass']
+      },
+      scripts: {
+        files: 'assets/javascript/*.js',
+        tasks: ['jshint'],
+        options: {
+          spawn: false
+        }
       }
     }
   });
+
+// on watch events configure jshint:all to only run on changed file
+ grunt.event.on('watch', function(action, filepath) {
+   grunt.config('jshint.all.src', filepath);
+ });
+
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');

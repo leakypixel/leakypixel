e5e79367a5a7ac1b5823895faf83faf49acf8280
Craig Fletcher <craig.fletcher@onthebeach.co.uk>
2014-07-04 16:46:07 +0100

    Add twitter feed to homepage.
    * Add utils.js
    * Add unstyled twitter feed as plain text
    * Remove twitter's own feed

---
+++ assets/javascript/utils.js
@@ -0,0 +1,61 @@
+var utils = (function() {
+  'use strict';
+  return {
+    getAJAX: function(args) {
+
+      if (!args.url) {
+        return;
+      }
+
+      var logToConsole = function(data) {
+        console.log(data);
+      };
+
+      args.onServerError = args.onServerError || logToConsole;
+      args.onRequestError = args.onRequestError || logToConsole;
+      args.onSuccess = args.onSuccess || logToConsole;
+
+      var request = new XMLHttpRequest();
+      request.open('GET', args.url, true);
+
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400){
+          // Success!
+          data = JSON.parse(request.responseText);
+          args.onSuccess(data);
+        } else {
+          // We reached our target server, but it returned an error
+          args.onServerError(request);
+        }
+      };
+
+      request.onerror = function() {
+        args.onRequestError(request);
+      };
+
+      request.send();
+    },
+
+    extend: function(out) {
+      out = out || {};
+
+      for (var i = 1; i < arguments.length; i++) {
+        if (!arguments[i])
+          continue;
+
+        for (var key in arguments[i]) {
+          if (arguments[i].hasOwnProperty(key))
+            out[key] = arguments[i][key];
+        }
+      }
+
+      return out;
+    },
+
+    onReady: function(func) {
+      document.addEventListener('DOMContentLoaded', function(){
+        func();
+      });
+    }
+  };
+})();

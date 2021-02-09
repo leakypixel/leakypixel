c40136b6283ac9aecffa180e7d0aba96572513a3
Craig Fletcher <hi@leakypixel.net>
2021-02-09 19:02:39 +0000

    Some work around getting blurbs

--- .npm-scripts/decorators/articleDecorator.js
+++ .npm-scripts/decorators/articleDecorator.js
@@ -1,9 +1,25 @@
 module.exports = function articleDecorator(config, item, meta) {
-  const introStart = item.content.indexOf("<p>") + 3;
+  const startIntroStrings = ["<p>", "<li>"];
+  let introStart = -1;
+  let i = 0;
+  while (introStart === -1 && i < startIntroStrings.length) {
+    introStart = item.content
+      .toString()
+      .indexOf(startIntroStrings[i].toString());
+    i++;
+  }
+  if (i < startIntroStrings.length) {
+    introStart = introStart + startIntroStrings[i].length - 1;
+  }
   const introEnd = item.content.indexOf("</p>");
   let cutoff;
   if (introEnd - introStart > config.cutoffLength) {
-    cutoff = item.content.indexOf("p>", introStart + config.cutoffLength) + 1;
+    cutoff = item.content.indexOf(".", introStart) + 1;
+    // Find the next end of sentence
+    const endChars = [" ", "<", "\n"];
+    while (!endChars.includes(item.content.charAt(cutoff))) {
+      cutoff = item.content.indexOf(".", cutoff) + 1;
+    }
   } else {
     cutoff = introEnd;
   }
@@ -11,6 +27,12 @@ module.exports = function articleDecorator(config, item, meta) {
   const titleStart = item.content.indexOf(">", item.content.indexOf("<h1")) + 1;
   const titleEnd = item.content.indexOf("</h1>");
   const title = item.content.substring(titleStart, titleEnd) || meta.name;
+
+  console.log("===================================");
+  console.log("Title:", title);
+  console.log("Blurb:", blurb);
+  console.log("Content:", JSON.stringify(item.content));
+  console.log("===================================");
   return {
     ...item,
     niceName: title,
